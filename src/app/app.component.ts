6594d400acdce46b7ecbd8984fe070f48693cf7a
merlinpratheesh <merlinpratheesh@gmail.com>
2020-12-08 17:39:54 +0530

    Error while async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {FormControl, Validators} from '@angular/forms';
 
+export interface collectiondoc{
+  fruits: string; 
+ 
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apitestertesting';
+
+  Testcollection: collectiondoc=
+  {
+    fruits : 'Apple'
+
+   }
+  constructor(){
+    const names$ = of(this.Testcollection);
+    names$.subscribe(
+      data1 => {
+        console.log(data1);
+      },
+
+      error => console.log(error),
+      () => console.log('Completed'),
+    );
+
+  }
 }
